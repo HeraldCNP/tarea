16ea79346ae6b0767c6df3b867833cd3fb08ae95
Herald <heraldcnp@gmail.com>
2019-04-05 09:49:08 -0400

    tarea1

--- tarea.js
+++ tarea.js
@@ -1,52 +1,78 @@
-function cinco(p,n){
-    n+=1;
-    var sum=p;
-    while(n--!=0){
-      sum*=10;
+function cinco(p, n) {
+    n += 1;
+    var sum = p;
+    while (n-- != 0) {
+        sum *= 10;
     }
-    sum+=p;
-    sum=Math.pow(sum,2);
-    p=0;
-    while(sum>0){
-      p+=sum%10;
-      sum=parseInt(sum/=10);
+    sum += p;
+    sum = Math.pow(sum, 2);
+    p = 0;
+    while (sum > 0) {
+        p += sum % 10;
+        sum = parseInt(sum /= 10);
     }
     return p;
 }
 
-function tres(cad){
-    var res='';
-    var res2='';
-    var i=0;
-    while(i<cad.length){
-        let c=cad.charAt(i);
-        if(c=='('){
-          i++;
-          while(c!=')'){
-            c=cad.charAt(i);
-            if(c!=')'){
-              res2+=c;
-              i++;
-            }
-          }
-          res2=volteo(res2);
-          res+=res2;
-        }else{
-          res+=c;
+
+
+var con = 0;
+
+function tres(cad) {
+    let res = '';
+    while (con < cad.length) {
+        let c = cad.charAt(con);
+        if (c == '(') {
+            con++;
+            res += add(cad);
+        } else {
+            res += c;
         }
-        i++;
+        con++;
     }
     return res;
 }
+const add = (cad) => {
+    //console.log("cadena recursiva "+cad);
+    let res = '';
+    while (con < cad.length) {
+        let c = cad.charAt(con);
+        if (c == '(') {
+            con++;
+            res += add(cad);
+        } else if (c == ')') {
+            res = volteo(res);
+            return res;
+        } else {
+            res += c;
+        }
+        con++;
+    }
+    return res;
 
-function volteo(cad){
-  var res='';
-  for(var i=cad.length-1;i>=0;i--){
-    let c=cad.charAt(i);
-    res+=c;
-  }
-  return res;
 }
 
-//console.log(cinco(5,1));
-console.log(tres("foo(bar)baz"));
+function volteo(cad) {
+    let sum = '';
+    for (let i = cad.length - 1; i >= 0; i--) {
+        let c = cad.charAt(i);
+        sum += c;
+    }
+    return sum;
+}
+
+
+function uno() {
+    var matrix = [
+        [0, 1, 1, 2],
+        [0, 5, 0, 0],
+        [2, 0, 3, 3]
+    ];
+    var sum = 0;
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 4; j++) {
+            sum += matrix[i][j];
+        }
+    }
+    return sum;
+}
